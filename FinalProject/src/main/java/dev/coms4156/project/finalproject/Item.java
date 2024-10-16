7114789ec167201e0334e055a2e280c1d4ca1ad2
ireneisabug <yc3841@columbia.edu>
2024-10-15 22:49:48 -0400

    chore:modified resource and item class;resource passed stylecheck.

--- FinalProject/src/main/java/dev/coms4156/project/finalproject/Item.java
+++ FinalProject/src/main/java/dev/coms4156/project/finalproject/Item.java
@@ -2,3 +2,9 @@ package dev.coms4156.project.finalproject;
 
 import java.io.*;
 import java.util.*;
+
+public class Item implements Serializable {
+  public String getStatus(){
+    return "";
+  }
+}
