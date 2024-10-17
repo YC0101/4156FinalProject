464462f97ca48ccf21c808eda2a1283e111329a3
ireneisabug <yc3841@columbia.edu>
2024-10-17 02:11:40 -0400

    chore: finished resource unit test, coverage reach 100%; modified resource's hashmap to adapt item class.

--- FinalProject/src/main/java/dev/coms4156/project/finalproject/Resource.java
+++ FinalProject/src/main/java/dev/coms4156/project/finalproject/Resource.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * and items offered.
  */
 public class Resource implements Serializable {
-  public Resource(HashMap<Integer, Item> items, String resourceId) {
+  public Resource(HashMap<String, Item> items, String resourceId) {
     this.items = items;
     this.resourceId = resourceId;
   }
@@ -22,7 +22,7 @@ public class Resource implements Serializable {
    * @param itemId              The ID of the new item.
    * @param item                The item object to add.
    */
-  public void addItem(int itemId, Item item) {
+  public void addItem(String itemId, Item item) {
     items.put(itemId, item);
   }
 
@@ -31,7 +31,7 @@ public class Resource implements Serializable {
    *
    * @return the hashmap containing all items.
    */
-  public HashMap<Integer, Item> getAllItems() {
+  public HashMap<String, Item> getAllItems() {
     return this.items;
   }
 
@@ -41,7 +41,7 @@ public class Resource implements Serializable {
    * @param itemId             The ID of the removing item.
    * @return the removed item.
    */
-  public Item removeItem(int itemId) {
+  public Item removeItem(String itemId) {
     Item removedItem = this.items.remove(itemId);
     return removedItem;
   }
@@ -52,7 +52,7 @@ public class Resource implements Serializable {
    * @param itemId             The ID of the removing item.
    * @return the item with the given itemId.
    */
-  public Item getItemById(int itemId) {
+  public Item getItemById(String itemId) {
     return this.items.get(itemId);
   }
 
@@ -63,8 +63,8 @@ public class Resource implements Serializable {
    */
   public String listAvailableItems() {
     StringBuilder result = new StringBuilder();
-    for (Map.Entry<Integer, Item> entry : items.entrySet()) {
-      int key = entry.getKey();
+    for (Map.Entry<String, Item> entry : items.entrySet()) {
+      String key = entry.getKey();
       Item value = entry.getValue();
       if (value.getStatus() == "available") {
         result.append(resourceId).append(" ").append(key).append(": ").append(value.toString())
@@ -82,8 +82,8 @@ public class Resource implements Serializable {
    */
   public String toString() {
     StringBuilder result = new StringBuilder();
-    for (Map.Entry<Integer, Item> entry : items.entrySet()) {
-      int key = entry.getKey();
+    for (Map.Entry<String, Item> entry : items.entrySet()) {
+      String key = entry.getKey();
       Item value = entry.getValue();
       result.append(resourceId).append(" ").append(key).append(": ").append(value.toString())
               .append("\n");
@@ -94,6 +94,6 @@ public class Resource implements Serializable {
 
   @Serial
   private static final long serialVersionUID = 234567L;
-  private HashMap<Integer, Item> items;
+  private HashMap<String, Item> items;
   private String resourceId;
 }
