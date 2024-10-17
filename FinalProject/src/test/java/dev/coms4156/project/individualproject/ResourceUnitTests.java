c9f379c8dd1d5d4198d3c8fbb159bf1fe88e8df8
Yanxi Chen <103548668+YC0101@users.noreply.github.com>
2024-10-17 09:52:53 -0400

    Chore/resource unit test (#17)
    * chore:add serialVersion to ResourceClass
    
    * chore: finished resource unit test, coverage reach 100%; modified resource's hashmap to adapt item class.

--- FinalProject/src/test/java/dev/coms4156/project/individualproject/ResourceUnitTests.java
+++ FinalProject/src/test/java/dev/coms4156/project/individualproject/ResourceUnitTests.java
@@ -26,12 +26,61 @@ public class ResourceUnitTests {
     testItem3 = new Item("Clothing", 0, null, "Donor789");  // No expiration, but quantity is zero
 
     testItemMap = new HashMap<>();
+    testItemMap.put(testItem.getItemId(), testItem);
+    testItemMap.put(testItem2.getItemId(), testItem2);
+    //testItemMap.put(testItem3.getItemId(),testItem3);
 
+    testItemMap2 = new HashMap<>();
+    testItemMap2.put(testItem.getItemId(), testItem);
+
+    testResource = new Resource(testItemMap, "testResource");
+    testResource2 = new Resource(testItemMap2, "testResource");
   }
 
-  /** The test course instance used for testing. */
+  @Test
+  public void testAddItem() {
+    testResource.addItem(testItem3.getItemId(), testItem3);
+    assertEquals(testResource.getItemById(testItem3.getItemId()), testItem3);
+  }
+
+  @Test
+  public void testGetAllItems() {
+    testItemMap.put(testItem3.getItemId(), testItem3);
+    assertEquals(testResource.getAllItems(), testItemMap);
+  }
+
+  @Test
+  public void testRemoveItem() {
+    testResource.removeItem(testItem3.getItemId());
+    assertEquals(testResource.getItemById(testItem3.getItemId()), null);
+  }
+
+  @Test
+  public void testGetItemById() {
+    assertEquals(testResource.getItemById(testItem2.getItemId()), testItem2);
+  }
+
+  @Test
+  public void testListAvailableItems() {
+    testResource.getItemById(testItem3.getItemId()).markAsDispatched();
+    testResource.getItemById(testItem2.getItemId()).markAsDispatched();
+    String result = "testResource " + testItem.getItemId() + ": " + testItem.toString() + "\n";
+    assertEquals(testResource.listAvailableItems(), result);
+  }
+
+  @Test
+  public void testToString() {
+    String result = "testResource " + testItem.getItemId() + ": " + testItem.toString() + "\n";
+    assertEquals(testResource2.toString(), result);
+  }
+
+
+  /** The test resource instance used for testing. */
   public static Item testItem;
   public static Item testItem2;
   public static Item testItem3;
-  public static HashMap<Integer, Item> testItemMap;
+  public static HashMap<String, Item> testItemMap;
+  public static HashMap<String, Item> testItemMap2;
+  public static Resource testResource;
+  public static Resource testResource2;
 }
